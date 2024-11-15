dea11602ab64cd50a1ebb596480333c9545b4204
Mandar Chitre <mandar@nus.edu.sg>
2024-11-16 00:39:26 +0800

    feat: add RLS

--- src/estimators.jl
+++ src/estimators.jl
@@ -1,5 +1,5 @@
-import LinearAlgebra: diagm
-export LMS, NLMS
+import LinearAlgebra: dot, diagm, mul!
+export LMS, NLMS, RLS
 
 ### LMS
 
@@ -16,7 +16,7 @@ LMS() = LMS(1f-2)
 
 Base.show(io::IO, alg::LMS) = print(io, "LMS($(repr(alg.μ)))")
 
-function update!(alg::LMS, p, estate, e, dy)
+function update!(alg::LMS, p, _, e, dy)
   @. p += alg.μ * dy * conj(e)
   abs2(e)
 end
@@ -36,7 +36,7 @@ NLMS() = NLMS(1f-1)
 
 Base.show(io::IO, alg::NLMS) = print(io, "NLMS($(repr(alg.μ)))")
 
-function update!(alg::NLMS, p, estate, e, dy)
+function update!(alg::NLMS, p, _, e, dy)
   sf = sum(abs2, dy)
   sf += eps(typeof(sf))
   @. p += alg.μ * dy * conj(e) / sf
@@ -45,26 +45,39 @@ end
 
 # ### RLS
 
-# """
-#     RLS(λ=0.99, σ=1.0)
+"""
+    RLS(λ=0.99, σ=1.0)
 
-# Recursive Least Squares (RLS) algorithm with forgetting factor λ and initial
-# covariance σ.
-# """
-# struct RLS{T} <: Estimator
-#   λ::T
-#   σ::T
-# end
+Recursive Least Squares (RLS) algorithm with forgetting factor λ and initial
+covariance σ.
+"""
+struct RLS{T} <: Estimator
+  λ::T
+  σ::T
+end
 
-# RLS() = RLS(0.99f0, 1f0)
-# RLS(λ) = RLS(λ, one(typeof(λ)))
+RLS() = RLS(0.99f0, 1f0)
+RLS(λ) = RLS(λ, one(typeof(λ)))
 
-# Base.show(io::IO, alg::RLS) = print(io, "RLS($(alg.λ), $(alg.σ)")
+Base.show(io::IO, alg::RLS) = print(io, "RLS($(alg.λ), $(alg.σ)")
 
-# setup(rng, alg::RLS, ps) = diagm(ones(eltype(ps), length(ps)) * alg.σ)
+function setup(rng, alg::RLS, p)
+  Rinv = diagm(ones(eltype(p), length(p)) * alg.σ)
+  # preallocate temporary storage for update!
+  k = similar(Rinv, length(p))
+  dyRinv = similar(Rinv, 1, length(p))
+  kdyRinv = similar(Rinv)
+  (k, Rinv, dyRinv, kdyRinv)
+end
 
-# function update!(alg::NLMS, ps, st, data, e, loss, dloss, t)
-#   #x?
-#   k = st * x ./ (alg.λ + x' * st * x)
-#   @. ps += k * e
-# end
+function update!(alg::RLS, p, estate, e, dy)
+  k, Rinv, dyRinv, kdyRinv = estate
+  mul!(k, Rinv, dy)
+  k ./= alg.λ + dot(dy, k)
+  @. p += k * conj(e)
+  mul!(dyRinv, dy', Rinv)
+  mul!(kdyRinv, k, dyRinv)
+  Rinv .-= kdyRinv
+  Rinv ./= alg.λ
+  abs2(e)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -15,6 +15,12 @@ include("test-comms.jl")
     channel_estimation(ComplexF32, [(0,0.8f0+0im), (7,0+0.3f0im), (11,-0.2f0-0.5f0im)], NLMS(1f0))
     channel_estimation(ComplexF64, [(0,0.8+0im), (7,0+0.3im), (11,-0.2-0.5im)], NLMS(1f0))
   end
+  @testset "RLS" begin
+    channel_estimation(Float32, [(0,0.8f0), (7,0.3f0), (11,-0.5f0)], RLS())
+    channel_estimation(Float64, [(0,0.8), (7,0.3), (11,-0.5)], RLS())
+    channel_estimation(ComplexF32, [(0,0.8f0+0im), (7,0+0.3f0im), (11,-0.2f0-0.5f0im)], RLS())
+    channel_estimation(ComplexF64, [(0,0.8+0im), (7,0+0.3im), (11,-0.2-0.5im)], RLS())
+  end
   @testset "options" begin
     channel_estimation(Float64, [(0,0.8), (7,0.3), (11,-0.5)], LMS(); nsteps=1000)
     channel_estimation(Float64, [(0,0.8), (7,0.3), (11,-0.5)], LMS(); saveat=1)
@@ -26,9 +32,11 @@ end
   @testset "Linear" begin
     channel_equalization([(0,0.8+0im), (7,0+0.3im), (11,-0.2-0.5im)], LinearModel, LMS(0.01))
     channel_equalization([(0,0.8+0im), (7,0+0.3im), (11,-0.2-0.5im)], LinearModel, NLMS())
+    channel_equalization([(0,0.8+0im), (7,0+0.3im), (11,-0.2-0.5im)], LinearModel, RLS())
   end
   @testset "DFE" begin
     channel_equalization([(0,0.8+0im), (7,0+0.3im), (11,-0.2-0.5im)], DFE, LMS(0.01))
     channel_equalization([(0,0.8+0im), (7,0+0.3im), (11,-0.2-0.5im)], DFE, NLMS())
+    channel_equalization([(0,0.8+0im), (7,0+0.3im), (11,-0.2-0.5im)], DFE, RLS())
   end
 end
